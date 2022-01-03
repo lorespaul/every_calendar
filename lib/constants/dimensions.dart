7b2995bffc639ad0714aad278c1086935100a305
Lorenzo Daneo <lorenzo.daneo91@gmail.com>
2022-01-03 13:56:10 +0100

    fix after rebase

--- lib/constants/dimensions.dart
+++ lib/constants/dimensions.dart
@@ -1,6 +1,6 @@
 class Dimensions {
   static const double appBarHeight = 50;
-  static const double bottomNavigationBarHeight = 55;
+  static const double bottomNavigationBarHeight = 90;
   static const double scaffoldTopAndBottomBarHeight =
       appBarHeight + bottomNavigationBarHeight;
 }
